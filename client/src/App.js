a738a0ec260746a1fab913a7225adaf918f29043
thedon122 <donovan122@yahoo.com>
2018-05-01 10:51:08 -0400

    started creating routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from '.components/HomePage'
+import ExistingUser from '.components/ExistingUser'
+import UserProfile from '.components/UseProfile'
+import NewUser from '.components/NewUser'
+import EventProfile from '.components/EventProfile'
+import NewEvent from '.components/NewEvent'
+import NewActivities from '.components/NewActivities'
+import ActivityProfile from '.components/ActivityProfile'
+import ActivitiesToDoList from '.components/ActivitiesToDoList'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+  class App extends Component {
+    render() {
+      return (
+        <Router>
+          <div>
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/:id" component={ExistingUser} />
+              <Route path="/" component={NewUser} />
+              <Route path="/:id" component={UserProfile} />
+            </Switch>
+          </div>
+        </Router>
+      );
+    }
   }
-}
 
 export default App;
