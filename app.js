0d2090c747b340b90050e1b914d5cc8a3710f3c5
thedon122 <donovan122@yahoo.com>
2018-04-30 20:21:49 -0400

    Created react app and build folder

--- app.js
+++ app.js
@@ -9,24 +9,27 @@ mongoose.connect(process.env.MONGODB_URI)
 
 const db = mongoose.connection
 db.on('error', err => {
-    console.log(err => {
-        console.log(err)
-    })
+  console.log(err => {
+    console.log(err)
+  })
 })
 
 db.on('open', () => {
-    console.log('Connected to MongoDB')
-  })
-  
-  app.use(logger('dev'))
-  app.use(bodyParser.json())
-  
-  app.get('/', (req, res) => {
-    res.send("Hello World")
-  })
-  
-  const PORT = process.env.PORT || 3001
-  
-  app.listen(PORT, () => {
-    console.log('App is up and running on port ' + PORT)
-  })
+  console.log('Connected to MongoDB')
+})
+app.use(express.static(`${__dirname}/client/build`))
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
+})
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+  console.log('App is up and running on port ' + PORT)
+})
