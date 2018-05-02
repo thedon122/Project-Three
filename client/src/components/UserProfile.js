75b20fe09cd1e5842e04337dc000118f12f6b19e
thedon122 <donovan122@yahoo.com>
2018-05-02 17:26:16 -0400

    fix route to get one user

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -4,38 +4,63 @@ import axios from 'axios'
 
 class UserProfile extends Component {
     state = {
-        lastName: '',
-        firstName: '',
-        favorite: '',
-        imgUrl: ''
+        user =[],
+        event =[]
     }
 
-    componentDidMount = () =>{
+    componentDidMount = () => {
         const userId = this.props.match.params.userId
         console.log("UserId from UserProfile component: ", userId)
         axios.get(`/api/users/${userId}`)
             .then(response => {
                 console.log(response.data)
                 this.setState({
-                    lastName: response.data.lastName,
-                    firstName: response.data.firstName,
-                    favorite: response.data.favorite,
-                    imgUrl: response.data.imgUrl
+                    lastName: response.user.lastName,
+                    firstName: response.user.firstName,
+                    favorite: response.user.favorite,
+                    imgUrl: response.user.imgUrl
                 })
             })
     }
-    
+
     render() {
-        console.log("RENDERING", this.state.lastName)
-        // const userIdividual = {
-        //     return (
-        //         <form>
-        //         </form>
-        //     )
-        // }
+        console.log("RENDERING", this.state.user)
+        userIdividual = () => {
+
+            <form>
+                <input
+                    type="text"
+                    name="firstName"
+                    value={this.state.firstName}
+                    onBlur={() => this.updateUser(idea)}
+                    onChange={(event) => this.handleChange(idea, event)} />
+
+                <textarea
+                    name="lastName"
+                    value={this.state.lastName}
+                    onBlur={() => this.updateUser(idea)}
+                    onChange={(event) => this.handleChange(idea, event)} />
+
+                <textarea
+                    name="favorite"
+                    value={this.state.favorite}
+                    onBlur={() => this.updateUser(idea)}
+                    onChange={(event) => this.handleChange(idea, event)} />
+
+                <textarea
+                    name="imgUrl"
+                    value={this.state.imgUrl}
+                    onBlur={() => this.updateUser(idea)}
+                    onChange={(event) => this.handleChange(idea, event)} />
+                <button
+                    onClick={() => { this.deleteIdea(idea._id) }}>
+                    Delete Idea
+          </button>
+            </form>
+        }
         return (
             <div>
-                 Name: {this.state.firstName} 
+
             </div >
         )
     }
